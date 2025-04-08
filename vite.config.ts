a9e5cca00c26d16f1460af86beadbd2962c062cd
Titov Igor <god94659@gmail.com>
2025-04-08 13:19:06 +0300

    бновлен базовый путь для нового репозитория

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/testpave/", // Базовый путь для GitHub Pages (имя репозитория)
+  base: "/testpavepo/",
 });
